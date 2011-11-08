5d796874fa29829ef361d70678d26cec0180fab7
Ryan Fischer <ryan.fischer@20spokes.com>
2011-11-08 10:32:11 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsMongoDBGeolocate::Application.config.secret_token = 'afa2628fd1a962711dc94f02cd2bd394f91d97288ac9f30fb2283f0d94a7d338293b24d01a91fb6286ed0bfba7c2dd22d7918e6b04f32d5e6cb73c7ef50e5f16'
